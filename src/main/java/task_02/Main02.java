b3ce384e36622706973365aa6485443ac41af0b0
EvgeniiBuller <buller.1@gmx.de>
2025-01-29 21:44:51 +0100

    Task 2

--- src/main/java/task_02/Main02.java
+++ src/main/java/task_02/Main02.java
@@ -1,6 +1,6 @@
 package task_02;
 
-public class Main_01 {
+public class Main02 {
 
     public static void main(String[] args) {
 
--- src/main/java/task_02/Main03.java
+++ src/main/java/task_02/Main03.java
@@ -1,6 +1,6 @@
 package task_02;
 
-public class Main_02 {
+public class Main03 {
 
     public static void main(String[] args) {
 
